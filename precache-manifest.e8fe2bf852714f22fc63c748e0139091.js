4f09a9877b579eb8e9c8920da46c2620f75adafa
Oscar <Oscarg_1991@yahoo.com>
2019-03-10 22:24:52 -0600

    Updates

---
+++ precache-manifest.e8fe2bf852714f22fc63c748e0139091.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  },
+  {
+    "revision": "9ce19fa93477935635cf",
+    "url": "/Clicky/static/js/main.d3574d0d.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/Clicky/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "9ce19fa93477935635cf",
+    "url": "/Clicky/static/css/main.3a949fda.chunk.css"
+  },
+  {
+    "revision": "11f3ec0cf09777dbf928bc8cd7076736",
+    "url": "/Clicky/index.html"
+  }
+];
